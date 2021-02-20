e6f8e3e8572c02da804236d4a2815d41edd9bfe9
Frayni Calderon <frayni10calderon@gmail.com>
2021-02-20 16:18:29 -0500

    Fix translation + Resize of image

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ esLang2.onclick = function () {
 	document.getElementById("title-p").innerHTML =
 		"Es un año nuevo y una nueva oportunidad para romper las reglas y brillar. Nuestra marca de labiales está diseñada para innovar fórmulas y ofrecerte productos de la mejor calidad, elaborados con los mejores ingredientes. Nuestro propósito es mezclar frescura y elegancia en cada Botella. Para que te sientas segura en tu propia piel.";
 	document.getElementById("title-mail").innerHTML =
-		"¡Suscríbete a nuestra lista de correo!";
+		"¡Unete a nuestra lista de correo!";
 	document.getElementById("cta-mail").innerHTML = "Unirse";
 	document.getElementById("cta-input").placeholder="Ingresa tu correo electronico";
 	document.getElementById("btn-nav1").innerHTML = "Únete";
@@ -55,7 +55,7 @@ enLang1.onclick = function () {
 	document.getElementById("title").innerHTML = "¡Coming soon!";
 	document.getElementById("title-p").innerHTML = "It's a new year and a new opportunity to break the rules and shine. Our lipstick brand is designed to innovate formulas and offer you the best quality products, made with the finest ingredients. Our purpose is to mix freshness and elegance in every bottle. So you can feel confident in your own skin.";
 	document.getElementById("title-mail").innerHTML =
-		"¡Subscribe to our mailing list!";
+		"¡Join to our mailing list!!";
 	document.getElementById("cta-mail").innerHTML = "Join";
 	document.getElementById("cta-input").placeholder="Enter your e-mail address";
 	document.getElementById("btn-nav1").innerHTML = "Join";
@@ -72,7 +72,7 @@ enLang2.onclick = function () {
 	document.getElementById("title").innerHTML = "¡Coming soon!";
 	document.getElementById("title-p").innerHTML = "It's a new year and a new opportunity to break the rules and shine. Our lipstick brand is designed to innovate formulas and offer you the best quality products, made with the finest ingredients. Our purpose is to mix freshness and elegance in every bottle. So you can feel confident in your own skin.";
 	document.getElementById("title-mail").innerHTML =
-		"¡Subscribe to our mailing list!";
+		"¡Join to our mailing list!!";
 	document.getElementById("cta-mail").innerHTML = "Join";
 	document.getElementById("cta-input").placeholder="Enter your e-mail address";
 	document.getElementById("btn-nav1").innerHTML = "Join";
