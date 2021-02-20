57636fed13a8f848e55210fb912df0bbbb5b02a7
Frayni Calderon <frayni10calderon@gmail.com>
2021-02-19 23:30:09 -0500

    Revert "Finish Landing page v2"
    This reverts commit 6cf296217e8532bc703d446b53357e01a0d3ce6d.

--- app.js
+++ app.js
@@ -1,52 +1,25 @@
-// Auto focus button
-function autoFocus() {
-	document.getElementById("cta-input").focus();
-}
+// Get the modal
+var modal = document.getElementById("myModal");
 
-// Translate website to spanish
-const esLang1 = document.getElementById("es-lang1");
-const enLang1 = document.getElementById("en-lang1");
-const esLang2 = document.getElementById("es-lang2");
-const enLang2 = document.getElementById("en-lang2");
+// Get the button that opens the modal
+var btn = document.getElementById("openModal");
 
-esLang1.onclick = function () {
-	document.getElementById("title").innerHTML = "¡Proximamente!";
-	document.getElementById("title-p").innerHTML =
-		"Es un año nuevo y una nueva oportunidad para romper las reglas y brillar. Nuestra marca de labiales está diseñada para innovar fórmulas y ofrecerte productos de la mejor calidad, elaborados con los mejores ingredientes. Nuestro propósito es mezclar frescura y elegancia en cada Botella. Para que te sientas segura en tu propia piel.";
-	document.getElementById("title-mail").innerHTML =
-		"¡Subscribe to our mailing list!";
-	document.getElementById("cta-mail").innerHTML = "Subscribirse";
-	document.getElementById("cta-input").placeholder="Ingresa tu correo electronico";
-	document.getElementById("btn-nav1").innerHTML = "Únete";
-	document.getElementById("btn-nav2").innerHTML = "Únete";
-};
-enLang1.onclick = function () {
-	document.getElementById("title").innerHTML = "¡Coming soon!";
-	document.getElementById("title-p").innerHTML = "It's a new year and a new opportunity to break the rules and shine. Our lipstick brand is designed to innovate formulas and offer you the best quality products, made with the finest ingredients. Our purpose is to mix freshness and elegance in every bottle. So you can feel confident in your own skin.";
-	document.getElementById("title-mail").innerHTML =
-		"¡Subscribe to our mailing list!";
-	document.getElementById("cta-mail").innerHTML = "Subscribe";
-	document.getElementById("btn-nav1").innerHTML = "Join";
-	document.getElementById("btn-nav2").innerHTML = "Join";
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+	modal.style.display = "block";
 };
 
-esLang2.onclick = function () {
-	document.getElementById("title").innerHTML = "¡Proximamente!";
-	document.getElementById("title-p").innerHTML =
-		"Es un año nuevo y una nueva oportunidad para romper las reglas y brillar. Nuestra marca de labiales está diseñada para innovar fórmulas y ofrecerte productos de la mejor calidad, elaborados con los mejores ingredientes. Nuestro propósito es mezclar frescura y elegancia en cada Botella. Para que te sientas segura en tu propia piel.";
-	document.getElementById("title-mail").innerHTML =
-		"¡Suscríbete a nuestra lista de correo!";
-	document.getElementById("cta-mail").innerHTML = "Subscribirse";
-	document.getElementById("cta-input").placeholder="Ingresa tu correo electronico";
-	document.getElementById("btn-nav1").innerHTML = "Únete";
-	document.getElementById("btn-nav2").innerHTML = "Únete";
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+	modal.style.display = "none";
 };
-enLang2.onclick = function () {
-	document.getElementById("title").innerHTML = "¡Coming soon!";
-	document.getElementById("title-p").innerHTML = "It's a new year and a new opportunity to break the rules and shine. Our lipstick brand is designed to innovate formulas and offer you the best quality products, made with the finest ingredients. Our purpose is to mix freshness and elegance in every bottle. So you can feel confident in your own skin.";
-	document.getElementById("title-mail").innerHTML =
-		"¡Subscribe to our mailing list!";
-	document.getElementById("cta-mail").innerHTML = "Subscribe";
-	document.getElementById("btn-nav1").innerHTML = "Join";
-	document.getElementById("btn-nav2").innerHTML = "Join";
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
 };
